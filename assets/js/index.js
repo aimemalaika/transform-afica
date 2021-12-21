10e7815452101b2816981c7039936c6f0aa6966a
Aime Malaika <aimemalaika1995@gmail.com>
2021-12-21 10:28:47 +0200

    Done with the mobile menue

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+const navigation = document.querySelector('.mobile-nav');
+const expandMenu = () => {
+  navigation.classList.remove('animate-left');
+  navigation.classList.add('animate-right');
+};
+
+const closeMenu = () => {
+  navigation.classList.remove('animate-right');
+  navigation.classList.add('animate-left');
+};
+
+document.querySelector('.open').addEventListener('click', expandMenu);
+
+const closeClass = document.querySelectorAll('.close');
+closeClass.forEach(((element) => element.addEventListener('click', closeMenu)));
