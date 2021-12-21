2010c56f3125c276ce2d9cdf18cbff6cce22635b
Aime Malaika <aimemalaika1995@gmail.com>
2021-12-21 16:37:25 +0200

    Add program section mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,49 @@ document.addEventListener('scroll', () => {
     header.classList.remove('white-bg');
   }
 });
+
+const mainPrograms = [
+  {
+    icon: 'icons8.png',
+    title: 'Artificial Intelligence for Africa',
+    description: 'Artificial Intelligence (AI) is a technology that can propel developing economies on a trajectory of sustainable development.',
+  },
+  {
+    icon: 'icons8.png',
+    title: 'AgriTech Blueprint for Africa',
+    description: 'As the most important sector of the African economy, agriculture has potential to reduce poverty on the continent, increase food security and improve nutrition.',
+  },
+  {
+    icon: 'icons8.png',
+    title: 'ICT Skills Capacity Building Blueprint',
+    description: 'The purpose of this blueprint is to develop a framework for ICT skills development and capacity building in Africa.',
+  },
+  {
+    icon: 'icons8.png',
+    title: 'Digital Economy',
+    description: 'This Blueprint seeks to provide a conceptual framework adopted by Kenya in its quest towards the realisation of a successful and sustainable digital economy.',
+  },
+  {
+    icon: 'icons8.png',
+    title: 'Digital ID',
+    description: 'Smart Africa is the bold and innovative commitment from African Heads of State and Government to accelerate',
+  },
+];
+
+mainPrograms.forEach((program) => {
+  document.querySelector('#main-program-grid').innerHTML += `
+    <li class="program-item">
+      <div class="program-icon">
+        <img src="./assets/images/${program.icon}" alt="">
+      </div>
+      <div class="program-title">
+        <h3>${program.title}</h3>
+      </div>
+      <div class="program-description">
+        <p>${program.description}</p>
+      </div>
+    </li>
+  `;
+});
+
+// document.querySelector('#main-program-grid').innerHTML += '';
